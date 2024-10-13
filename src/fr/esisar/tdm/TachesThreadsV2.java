1973c3f934e7a28e0640654b4fc2b79b5a6100a2
José Neto <jose.silva.neto@itec.ufpa.br>
2024-10-13 20:51:26 +0200

    threads in question 4 missing orchestration

--- src/fr/esisar/tdm/TachesThreadsV2.java
+++ src/fr/esisar/tdm/TachesThreadsV2.java
@@ -1,11 +1,11 @@
 package fr.esisar.tdm;
 
 public class TachesThreadsV2 extends Thread {
-    public int tacheChiffre;
+    public String tacheChiffre;
     public int delai = 2000;
     public int iterations;
 
-    public TachesThreadsV2(int tacheChiffre, int iterations) {
+    public TachesThreadsV2(String tacheChiffre, int iterations) {
         this.tacheChiffre = tacheChiffre;
         this.iterations = iterations;
     }
@@ -22,13 +22,13 @@ public class TachesThreadsV2 extends Thread {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        TachesThreadsV2 t1 = new TachesThreadsV2(1, 5);
-        TachesThreadsV2 t2 = new TachesThreadsV2(2, 10);
-        TachesThreadsV2 t3 = new TachesThreadsV2(3, 10);
-        TachesThreadsV2 t4 = new TachesThreadsV2(4, 20);
-        TachesThreadsV2 t5 = new TachesThreadsV2(5, 15);
-        TachesThreadsV2 t6 = new TachesThreadsV2(6, 3);
-        TachesThreadsV2 t7 = new TachesThreadsV2(6, 7);
+        TachesThreadsV2 t1 = new TachesThreadsV2("cebola", 5);
+        TachesThreadsV2 t2 = new TachesThreadsV2("calango", 9);
+        TachesThreadsV2 t3 = new TachesThreadsV2("batman", 3);
+        TachesThreadsV2 t4 = new TachesThreadsV2("carro", 8);
+        TachesThreadsV2 t5 = new TachesThreadsV2("tralhoto", 10);
+        TachesThreadsV2 t6 = new TachesThreadsV2("playstation", 8);
+        TachesThreadsV2 t7 = new TachesThreadsV2("boa vista", 7);
 
         t1.start();
         t1.join();
@@ -36,14 +36,27 @@ public class TachesThreadsV2 extends Thread {
         t2.start();
         t3.start();
         t4.start();
-        t2.join();
-        t3.join();
 
-        t5.start();
-        t5.join();
-        t4.join();
+        if (t2.isAlive() || t3.isAlive()) {
+            t2.join();
+            t3.join();
+        } else if (!t2.isAlive() && !t3.isAlive()) {
+            t5.start();
+        }
 
-        t6.start();
+        if (t4.isAlive()) {
+            t4.join();
+        } else {
+            t6.start();
+        }
+
+        if (t5.isAlive() || t6.isAlive()) {
+            t5.join();
+            t6.join();
+        } else if (!t5.isAlive() && !t6.isAlive()) {
+            t7.start();
+            t7.join();
+        }
     }
 
 }
